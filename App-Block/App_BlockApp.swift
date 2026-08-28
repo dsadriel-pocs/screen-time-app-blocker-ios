224931b378834dcda0158915cd6fd0932041a48d
Adriel de Souza <37272343+dsadriel@users.noreply.github.com>
2026-08-28 15:29:10 -0300

    Initial Commit

---
+++ App-Block/App_BlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_BlockApp.swift
+//  App-Block
+//
+//  Created by Adriel de Souza on 28/08/26.
+//
+
+import SwiftUI
+
+@main
+struct App_BlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App-Block/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App-Block
+//
+//  Created by Adriel de Souza on 28/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
